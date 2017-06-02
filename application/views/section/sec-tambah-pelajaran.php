f22dce456e0f6070e2598038002d05f4eb43d813
friendlysipayung <friendly.1301144001@gmail.com>
2017-06-02 16:19:59 +0700

    + change admin layout (use tabulation at section) + add admin section : tambah pelajaran,student, and instructor; view student and instructor.

--- application/views/section/sec-tambah-pelajaran.php
+++ application/views/section/sec-tambah-pelajaran.php
@@ -8,12 +8,12 @@
 ?>
 <section class="content-header">
     <h1>
-        Add Student
+        Add Pelajaran
         <small> </small>
     </h1>
     <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active">Add Student</li>
+        <li class="active">Add Pelajaran</li>
     </ol>
 </section>
 <section class="content">
