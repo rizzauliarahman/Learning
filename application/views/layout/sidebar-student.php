3919f7e8406ffaf4145c54425dd6f30565a41020
Renesje <ranarenes.rengga@gmail.com>
2017-06-08 12:11:44 +0700

    versi 1

---
+++ application/views/layout/sidebar-student.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 02/06/2017
+ * Time: 10:55
+ */
+?>
+
+<!-- Left side column. contains the sidebar -->
+<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+        <!-- Sidebar user panel -->
+        <div class="user-panel">
+            <div class="pull-left image">
+                <img src="<?php echo base_url('assets/ext/AdminLTE-2.0.5/dist/img/user2-160x160.jpg') ?>" class="img-circle" alt="User Image" />
+            </div>
+            <div class="pull-left info">
+                <p><?php echo $this->session->userdata("nama"); ?></p>
+
+                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+            </div>
+        </div>
+        <!-- search form -->
+        <form action="#" method="get" class="sidebar-form">
+            <div class="input-group">
+                <input type="text" name="q" class="form-control" placeholder="Search..."/>
+                <span class="input-group-btn">
+                    <button type='submit' name='search' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i></button>
+                </span>
+            </div>
+        </form>
+        <!-- /.search form -->
+        <!-- sidebar menu: : style can be found in sidebar.less -->
+        <ul class="sidebar-menu">
+            <li class="header">MAIN NAVIGATION</li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-book"></i> <span>Pelajaran</span> <i class="fa fa-angle-left pull-right"></i>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="<?php echo site_url('#') ?>"><i class="fa fa-circle-o"></i>Task</a></li>
+                    <li><a href="<?php echo site_url('#') ?>"><i class="fa fa-circle-o"></i>History</a></li>
+<!--
+                    Bagian ini di komen dulu, student kayanya gaperlu data jawaban?
+
+                    <li><a href="--><?php //echo site_url('#') ?><!--"><i class="fa fa-circle-o"></i> Data Jawaban</a></li>
+
+-->             </ul>
+            </li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-gears"></i> <span>Settings</span> <i class="fa fa-angle-left pull-right"></i>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="<?php echo site_url('#') ?>"><i class="fa fa-circle-o"></i>Account</a></li>
+                    <li><a href="<?php echo site_url('#') ?>"><i class="fa fa-circle-o"></i>Preferences</a></li>
+                </ul>
+            </li>
+
+
+    </section>
+    <!-- /.sidebar -->
+</aside>
+
+<!-- =============================================== -->
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
