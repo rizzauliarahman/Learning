c1f2cb63b5f8920e3d5d3fbd6442ef39fd4ac2c5
rizzauliarahman <rizzauliarahman@gmail.com>
2017-06-19 21:10:07 +0700

    Fourth Commit

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -8,79 +8,90 @@
  */
 class Admin extends CI_Controller
 {
-    function __construct(){
+    function __construct()
+    {
         parent::__construct();
         $this->load->model('user');
         $this->load->model('course');
         $this->load->model('kategori');
-        
-        if($this->session->userdata('status') != "login"){
+
+        if ($this->session->userdata('status') != "login") {
             redirect(base_url("login"));
         }
     }
 
-    function index(){
-        if ($this->session->userdata("nama") != ""){
+    function index()
+    {
+        if ($this->session->userdata("nama") != "") {
             $this->load->view('admin');
-        }else{
+        } else {
             redirect('home');
         }
 
     }
 
-    function get_all_user($table){
+    function get_all_user($table)
+    {
         return $this->user->get_all($table);
     }
 
-    function delete_user($table, $id){
-        $identifier = $table.'_id';
+    function delete_user($table, $id)
+    {
+        $identifier = $table . '_id';
         $where = array($identifier => $id);
 
         $this->user->delete_user($table, $where);
     }
 
-    function get_user($table, $id){
-        $identifier = $table.'_id';
+    function get_user($table, $id)
+    {
+        $identifier = $table . '_id';
         $where = array($identifier => $id);
 
         return $this->user->get_user($table, $where);
     }
 
-    function get_list_kategori(){
+    function get_list_kategori()
+    {
         $query = $this->kategori->get_all_kategori();
         $row = $query->result();
 
         return $row;
     }
 
-    function insert_kategori(){
+    function insert_kategori()
+    {
         $nama = $this->input->post('nama');
 
         $data = array('kategori_name' => $nama);
         $this->kategori->insert_kategori($data);
     }
 
-    function delete_kategori($id){
+    function delete_kategori($id)
+    {
         $where = array('kategori_id' => $id);
 
         $this->kategori->delete_kategori($where);
     }
 
-    function update_kategori($id){
+    function update_kategori($id)
+    {
         $where = array('kategori_id' => $id);
         $data = array('kategori_id' => $id, 'kategori_name' => $this->input->post('nama'));
 
         $this->kategori->update_kategori($data, $where);
     }
 
-    function get_kategori($name){
+    function get_kategori($name)
+    {
         $where = array('kategori_name' => $name);
 
         $row = $this->kategori->get_kategori($where);
         return $row;
     }
 
-    function get_list_course(){
+    function get_list_course()
+    {
         $kategori = $this->kategori->get_all_kategori();
         $course = array();
 
@@ -88,7 +99,7 @@ class Admin extends CI_Controller
             $where_kategori = array('kategori_id' => $k->kategori_id);
             $query = $this->course->get_kategori_course($where_kategori);
 
-            foreach ($query as $q){
+            foreach ($query as $q) {
                 $where_course = array('course_id' => $q->course_id);
                 $course_query = $this->course->get_course($where_course);
 
@@ -99,20 +110,22 @@ class Admin extends CI_Controller
         return course;
     }
 
-    function get_instructor_course($instructor_id){
+    function get_instructor_course($instructor_id)
+    {
         $where = array('instructor_id' => $instructor_id);
         $result = $this->course->get_instructor_course($where);
 
         return $result;
     }
 
-    function get_kategori_course($kategori_id){
+    function get_kategori_course($kategori_id)
+    {
         $where_kategori = array('kategori_id' => $kategori_id);
         $course = array();
 
         $result = $this->course->get_category_course($where_kategori);
 
-        foreach ($result as $r){
+        foreach ($result as $r) {
             $where_course = array('course_id' => $r->course_id);
 
             $row = $this->coourse->get_course($where_course);
@@ -120,6 +133,45 @@ class Admin extends CI_Controller
         }
 
         return course;
+    }
+
+    function insert_pelajaran()
+    {
+        $config['upload_path'] = FCPATH . 'assets/files/';
+        $this->load->library('upload', $config);
+        $this->upload->initialize($config);
+
+        $data = array(
+            'Title' => $this->input->post('judul'),
+            'Price' => $this->input->post('harga'),
+            'Article' => $this->input->post('article')
+        );
+
+        $this->upload->do_upload('file');
+        echo $this->upload->display_errors();
+        $file = $this->upload->data();
+        $data_2 = array(
+            'Course_ID' => ' ',
+            'Link' => $file['full_path']
+        );
+
+        foreach ($file as $item => $value) {
+            echo $item . ' : ' . $value . '<br>';
+        }
+
+        $result = $this->course->insert_course($data);
+        /* $result_2 = $this->chapter->insert_chapter($data_2);
+
+        if ($result && $result_2){
+             echo '<script language="javascript">alert("Artikel Berhasil Di Post")</script>';
+             echo '<script language="javascript">window.location =""'.base_url('admin') ; echo ' </script>';
+         }
+         else {
+             echo '<script language="javascript">alert("Error : Artikel Gagal Di Post")</script>';
+             echo '<script language="javascript">window.location =""'.base_url('admin') ; echo ' </script>';
+         }
+       */
+
     }
 
 }
