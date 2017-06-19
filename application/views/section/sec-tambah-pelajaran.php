8507748b599e9a99b156e198a55a19126ff3d58e
friendlysipayung <friendly.1301144001@gmail.com>
2017-06-19 10:40:21 +0700

    # update"

--- application/views/section/sec-tambah-pelajaran.php
+++ application/views/section/sec-tambah-pelajaran.php
@@ -29,31 +29,41 @@
             <div class="box box-primary">
                 <div class="col-lg-12">
                     <p></p>
-                    <form action="" method="post">
+                    <form action="<?php echo base_url('admin/insert_pelajaran'); ?>" method="post">
                         <div class="row">
                             <div class="col-lg-6">
                                 <div class="box-body pad">
                                 <div class="form-group has-feedback">
-                                    <input type="text" class="form-control" placeholder="Nama Pelajaran" name="nama">
+                                    <input type="text" class="form-control" placeholder="Judul Pelajaran" name="judul">
                                     <span class="glyphicon glyphicon-edit form-control-feedback"></span>
                                 </div>
+
+                                <!-- HARUSNYA MULTI UPLOAD FILE -->
                                 <div class="form-group has-feedback">
-                                    <input type="text" class="form-control" placeholder="Kategori" name="kategori">
-                                    <span class="glyphicon glyphicon-edit form-control-feedback"></span>
-                                </div>
-                                <div class="form-group has-feedback">
-                                    <input type="text" class="form-control" placeholder="Tags" name="tag">
-                                    <span class="glyphicon glyphicon-edit form-control-feedback"></span>
+                                    <input type="file" class="form-control" name="file">
                                 </div>
                                 </div>
                             </div>
+                            <div class="col-lg-offset-1 col-lg-4">
+                                <div class="box-body pad">
+                                    <div class="form-group has-feedback">
+                                        <input type="text" class="form-control" placeholder="Kategori" name="kategori">
+                                        <span class="glyphicon glyphicon-edit form-control-feedback"></span>
+                                    </div>
+                                    <div class="form-group has-feedback">
+                                            <input type="text" class="form-control" placeholder="Harga" name="harga">
+                                            <span class="glyphicon glyphicon-edit form-control-feedback"></span>
+                                    </div>
+                                </div>
+
+                            </div>
                         </div>
                         <div class="box-body pad">
-                             <textarea class="textarea" placeholder="Place some text here" style="width: 90%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"></textarea>
+                             <textarea class="textarea" placeholder="Place some text here" rows="10" cols="15" name="article" style="width: 90%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"></textarea>
                         </div>
 
                         <div class="col-xs-2">
-                            <button type="submit" class="btn btn-primary btn-block btn-flat">Submit</button>
+                            <button type="submit" value="upload" class="btn btn-primary btn-block btn-flat">Submit</button>
                         </div>
                             <!-- /.col -->
 
